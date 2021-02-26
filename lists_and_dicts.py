fa97153fb3f15d7bfaa66b03f439e7127afd3604
Facundo García Martoni <thunderxforce@gmail.com>
2021-02-26 18:06:55 -0300

    Clase: Listas y diccionarios anidados - Finalizada

---
+++ lists_and_dicts.py
@@ -0,0 +1,24 @@
+def run():
+    my_list = [1, "Hello", True, 4.5]
+    my_dict = {"firstname": "Facundo", "lastname": "García"}
+
+    super_list = [
+        {"firstname": "Facundo", "lastname": "García"},
+        {"firstname": "Miguel", "lastname": "Rodriguez"},
+        {"firstname": "Pablo", "lastname": "Trinidad"},
+        {"firstname": "Susana", "lastname": "Martinez"},
+        {"firstname": "José", "lastname": "Fernandez"},
+    ]
+
+    super_dict = {
+        "natural_nums": [1, 2, 3, 4, 5],
+        "integer_nums": [-1, -2, 3, 0, 1],
+        "floating_nums": [1.1, 4.55, 6.43],
+    }
+
+    for key, value in super_dict.items():
+        print(key, ">", value)
+
+
+if __name__ == '__main__':
+    run()
