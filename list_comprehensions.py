60371b7dbb60c7abbd5bb1f32928442eb3811f87
Facundo García Martoni <thunderxforce@gmail.com>
2021-02-26 18:24:46 -0300

    List comprehension

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,9 +1,11 @@
 def run():
-    squares = []
+    # squares = []
 
-    for i in range(1, 101):
-        if i % 3 != 0:
-            squares.append(i**2)
+    # for i in range(1, 101):
+    #     if i % 3 != 0:
+    #         squares.append(i**2)
+
+    squares = [i**2 for i in range(1, 101) if i %3 != 0]
     
     print(squares)
 
