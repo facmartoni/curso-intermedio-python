055e4e945776b0f2c41fa389d29becf654ff1307
Facundo García Martoni <thunderxforce@gmail.com>
2021-02-26 19:13:42 -0300

    Dictionary comprehension

--- dict_comprehensions.py
+++ dict_comprehensions.py
@@ -1,10 +1,13 @@
 def run():
-    my_dict = {}
+    # my_dict = {}
 
-    for i in range(1, 101):
-        if i % 3 !=0:
-            my_dict[i] = i**2
+    # for i in range(1, 101):
+    #     if i % 3 !=0:
+    #         my_dict[i] = i**2
 
+    # print(my_dict)
+
+    my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
     print(my_dict)
 
 
